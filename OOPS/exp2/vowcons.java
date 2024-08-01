8e2080851b02a8e4f7e213ed5684ea20bd3b94c0
Dinesh reddy <168610030+Dineshreddy-13@users.noreply.github.com>
2024-08-01 10:40:26 +0530

    Update exp2

---
+++ OOPS/exp2/vowcons.java
@@ -0,0 +1,20 @@
+package lab2;
+public class vow_con {
+   public static void main(String[] args) {
+       String inp_String = "NIVESH";
+       int vow = 0;
+       int con = 0;
+       inp_String = inp_String.toLowerCase();
+       for (char c : inp_String.toCharArray()) {
+           if (c >= 'a' && c <= 'z') {
+               if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                vow++;
+               } else {
+                con++;
+               }
+           }
+       }
+       System.out.println("Number of vowels: " + vow);
+       System.out.println("Number of consonants: " + con);
+   }
+}
