b7efc68233a56de962f25e557101dc03ce3a0c2d
Dineshreddy <168610030+Dineshreddy-13@users.noreply.github.com>
2024-08-08 19:06:51 +0530

    Update exp3.py

--- DSA/exp3/exp3.py
+++ DSA/exp3/exp3.py
@@ -14,15 +14,14 @@ class Queue:
                 self.front = 0
             self.rear +=1
             self.queue[self.rear] = data
+            print("Enqueue successfull")
+            
     def isFull(self):
-        if self.rear == self.max-1:
-            return 1
-        else:
-            return 0
+        return self.rear == self.max-1
    
     def Dequeue(self):
         if(self.isEmpty()):
-            print("empty")
+            print("Queue is empty")
         else:
             x = self.queue[self.front]
             if self.front == self.rear:
@@ -31,29 +30,40 @@ class Queue:
                 self.front = 0
             else:
                 self.front +=1
+            print("Dequeue successfull")
             return x
        
     def isEmpty(self):
-        if (self.front == -1 ):
-            return 1
-        else:
-            return 0
+            return self.front == -1
        
     def Display(self):
+        print()
         for i in range(self.front,self.rear+1):
             print(self.queue[i],end = " ")
+        print()
            
-obj = Queue(5,"Circular")
-obj.Enqueue(1)
-obj.Enqueue(2)
-obj.Enqueue(3)
-obj.Enqueue(4)
-obj.Enqueue(4)
-obj.Dequeue()
-obj.Dequeue()
-obj.Dequeue()
-obj.Dequeue()
-obj.Dequeue()
-obj.Enqueue(5)
 
-obj.Display()
+while(True):
+    print("1.linear queue\n2.CircularQueue\n3.Exit")
+    x= int(input("enter your choice : "))
+    if x == 1:
+        size = int(input("enter the size of the queue : "))
+        obj = Queue(size)
+    elif x == 2:
+        size = int(input("enter the size of the queue : "))
+        obj = Queue(size,"circular")
+    else:
+        break
+    while(True):
+        print("\n1.Enqueue\n2.Dequeue\n3.Display\n4.Exit")
+        b= int(input("enter your choice : "))
+        if b==1:
+            data = input("enter The data : ")
+            obj.Enqueue(data)
+        elif b==2:
+            obj.Dequeue()
+        elif b==3:
+            obj.Display()
+        else :
+            break
+
