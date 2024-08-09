778493d2c150a2650e3c438e54abfe0384ef5577
Dineshreddy <168610030+Dineshreddy-13@users.noreply.github.com>
2024-08-09 17:36:15 +0530

    updated exp3

--- DSA/exp3/exp3.py
+++ DSA/exp3/exp3.py
@@ -1,8 +1,15 @@
 class Queue:
+<<<<<<< Updated upstream
     def __init__(self, max_size, queue_type="linear"):
         self.queue = [None] * max_size
         self.type = queue_type.lower()
         self.max = max_size
+=======
+    def __init__(self, max, type="linear"):
+        self.queue = [None] * max
+        self.type = type.lower()
+        self.max = max
+>>>>>>> Stashed changes
         self.front = -1
         self.rear = -1
 
@@ -17,7 +24,11 @@ class Queue:
             else:
                 self.rear += 1
             self.queue[self.rear] = data
+<<<<<<< Updated upstream
             print("Enqueue successful")
+=======
+            print("Enqueue successfull --> "+data)
+>>>>>>> Stashed changes
 
     def isFull(self):
         if self.type == "circular":
@@ -35,7 +46,11 @@ class Queue:
                 self.front = (self.front + 1) % self.max
             else:
                 self.front += 1
+<<<<<<< Updated upstream
             print("Dequeue successful")
+=======
+            print("Dequeue successfull --> "+x)
+>>>>>>> Stashed changes
             return x
 
     def isEmpty(self):
@@ -58,17 +73,26 @@ class Queue:
 
 while True:
     print("1. Linear Queue\n2. Circular Queue\n3. Exit")
+<<<<<<< Updated upstream
     choice = int(input("Enter your choice: "))
     if choice == 1:
         size = int(input("Enter the size of the queue: "))
         obj = Queue(size)
     elif choice == 2:
+=======
+    x = int(input("Enter your choice: "))
+    if x == 1:
+        size = int(input("Enter the size of the queue: "))
+        obj = Queue(size)
+    elif x == 2:
+>>>>>>> Stashed changes
         size = int(input("Enter the size of the queue: "))
         obj = Queue(size, "circular")
     else:
         break
     while True:
         print("\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit")
+<<<<<<< Updated upstream
         action = int(input("Enter your choice: "))
         if action == 1:
             data = input("Enter the data: ")
@@ -76,6 +100,18 @@ while True:
         elif action == 2:
             obj.Dequeue()
         elif action == 3:
+=======
+        a = int(input("Enter your choice: "))
+        if a == 1:
+            data = input("Enter the data: ").split(" ")
+            for i in data:
+                obj.Enqueue(i)
+        elif a == 2:
+            d = int(input("enter no of times to Dequeue: "))
+            for i in range (d):
+                obj.Dequeue()
+        elif a == 3:
+>>>>>>> Stashed changes
             obj.Display()
         else:
             break
--- OOPS/exp1/GradeGenerator.java
+++ OOPS/exp1/GradeGenerator.java
@@ -1,32 +1,30 @@
 import java.util.Scanner;
-
 public class GradeGenerator {
-            public static void main(String[] args) {
-			Scanner input = new Scanner(System.in);
-			System.out.println("<---------------------------->");
-			System.out.println("| DINESH REDDY - URK23CS1263 |");
-			System.out.println("<---------------------------->\n");
-            System.out.println("Enter subjects marks : ");
-			float physics = input.nextFloat();
-			float chemistry = input.nextFloat();
-			float biology = input.nextFloat();
-			float mathematics = input.nextFloat();
-			float computer = input.nextFloat();
-			float percentage = ((physics + chemistry + biology + mathematics + computer)/500)*100;
-			System.out.println("percentage : " +percentage);
-			if (percentage >= 90)
-				System.out.println("Grade A");
-			else if (percentage >= 80 && percentage < 90)
-				System.out.println("Grade B");
-			else if (percentage >= 70 && percentage < 80)
-				System.out.println("Grade C");
-			else if (percentage >= 60 && percentage < 70)
-				System.out.println("Grade D");
-			else if (percentage >= 40 && percentage < 60)
-				System.out.println("Grade E");
-			else
-				System.out.println("Grade F");
-			input.close();
-	}	
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("<---------------------------->");
+        System.out.println("| DINESH REDDY - URK23CS1263 |");
+        System.out.println("<---------------------------->\n");
+        System.out.println("Enter subjects marks : ");
+        float physics = input.nextFloat();
+        float chemistry = input.nextFloat();
+        float biology = input.nextFloat();
+        float mathematics = input.nextFloat();
+        float computer = input.nextFloat();
+        float percentage = ((physics + chemistry + biology + mathematics + computer)/500)*100;
+        System.out.println("percentage : " +percentage);
+        if (percentage >= 90)
+            System.out.println("Grade A");
+        else if (percentage >= 80 && percentage < 90)
+            System.out.println("Grade B");
+        else if (percentage >= 70 && percentage < 80)
+            System.out.println("Grade C");
+        else if (percentage >= 60 && percentage < 70)
+            System.out.println("Grade D");
+        else if (percentage >= 40 && percentage < 60)
+            System.out.println("Grade E");
+        else
+            System.out.println("Grade F");
+        input.close();
+}	
 }
