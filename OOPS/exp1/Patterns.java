f03ccb8e11a08ab0f2ae066f0276f24e50e527cd
Dinesh reddy <168610030+Dineshreddy-13@users.noreply.github.com>
2024-08-14 13:29:21 +0530

    Update Patterns.java

--- OOPS/exp1/Patterns.java
+++ OOPS/exp1/Patterns.java
@@ -1,9 +1,8 @@
 
 public class Patterns {
     public static void main(String[] args) {
-        System.out.println("<---------------------------->");
-        System.out.println("| DINESH REDDY - URK23CS1263 |");
-        System.out.println("<---------------------------->\n");
+        System.out.println("<---------------------------->\n| DINESH REDDY - URK23CS1263 |\n<---------------------------->\n");
+
         for (int i=1; i<=4; i++){
             for (int j=3; j>=i; j--){
                 System.out.print("  ");
