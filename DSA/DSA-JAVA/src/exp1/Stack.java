19248db4a80580c7f128fef0a04210991aa42542
Dinesh reddy <168610030+Dineshreddy-13@users.noreply.github.com>
2024-09-14 09:16:28 +0530

    updated

---
+++ DSA/DSA-JAVA/src/exp1/Stack.java
@@ -0,0 +1,76 @@
+package exp1;
+import java.util.Scanner;
+
+public class Stack {
+    int arr[],top,max;
+
+    Stack(int size){
+        arr = new int[size];
+        top = -1;
+        max = size;
+    }
+
+    public void push(int data){
+        if (isFull()){
+            System.out.println("--> Stack overflow");
+            return;
+        }
+        arr[++top] = data;
+    }
+
+    public boolean isFull(){
+        return top == max - 1;
+    }
+
+    public int pop(){
+        if (isEmpty()){
+            System.out.println("stack underflow");
+            return -1;
+        }
+        int x = arr[top--];
+        return x;
+    }
+
+    public boolean isEmpty(){
+        return top == -1;
+    }
+
+    public int peek(){
+        return arr[top];
+    }
+
+    public void display(){
+        if (isEmpty())
+            System.out.println("--> stack underflow");
+            for (int i = top; i>=0; i--){
+                System.out.println("|_" + arr[i] + "_|");
+            }
+    }
+
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the stack : ");
+        Stack s1 = new Stack(sc.nextInt());
+        boolean loop = true;
+        do{
+            System.out.println("\nMenu : \n1.push\n2.pop\n3.peek\n4.display\n5.Exit");
+            System.out.print("enter your choice : ");
+            switch(sc.nextInt()){
+                case 1 -> {
+                    System.out.print("Enter the no of elements to be pushed : ");
+                    int n = sc.nextInt();
+                    System.out.print("Enter the elements : ");
+                    for(int i = 0; i < n; i++){
+                        s1.push(sc.nextInt());
+                    }
+                }
+                case 2 -> s1.pop();
+                case 3 -> s1.peek();
+                case 4 -> s1.display();
+                case 5 -> loop = false;
+                default -> System.out.println("Invalid choice");
+            }
+        }while(loop);
+        sc.close();
+    }   
+}
---
+++ DSA/DSA-JAVA/src/exp8/LinearAndBinarySearch.java
@@ -0,0 +1,60 @@
+package exp8;
+import java.util.Scanner;
+
+public class LinearAndBinarySearch {
+
+    public static int linearSearch(int arr[],int key){
+        for (int i = 0; i<arr.length; i++) {
+            if (arr[i] == key)
+                return i;
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int arr[],int key) {
+        int first = 0;
+        int last = arr.length-1;
+        while(first<=last) {
+            int mid_index = (first + last)/2;
+
+            if (arr[mid_index] == key) 
+                return mid_index;
+            if (key < arr[mid_index]) 
+                last = mid_index - 1;
+            else 
+                first = mid_index + 1;  
+        }
+        return -1;
+    }
+
+    static int[] arrayInput(Scanner sc) {
+        System.out.print("enter array size : ");
+        int arr[] = new int[sc.nextInt()];
+        System.out.print("enter elements : ");
+        for(int i = 0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+        
+    static int inputKey(Scanner sc) {
+        System.out.print("enter element to be searched : ");
+        return sc.nextInt();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        do {
+            System.out.println("Menu : \n1.LinearSearch\n2.BinarySearch\n3.exit");
+            System.out.print("choose search : ");
+            switch(sc.nextInt()) {
+                case 1 -> System.out.println("index : " + linearSearch(arrayInput(sc),inputKey(sc)));
+                case 2 -> System.out.println("index : " + binarySearch(arrayInput(sc),inputKey(sc)));
+                case 3 -> loop = false;
+                default -> System.out.println("invalid");
+        }
+        }while(loop);
+        sc.close();
+    }
+}
