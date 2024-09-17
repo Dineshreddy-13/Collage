ec9f99e9931cf4ad495e01aba330911ab002a4f4
Dinesh reddy <168610030+Dineshreddy-13@users.noreply.github.com>
2024-09-17 09:40:37 +0530

    added newfiles

---
+++ SEM 3/DSA-LAB/DSA-JAVA/src/exp4/CircularQueue.java
@@ -0,0 +1,60 @@
+package exp4;
+
+public class CircularQueue {
+    int arr[];
+    int front,rear,max,size;
+    CircularQueue(int capacity){
+        arr = new int[capacity];
+        front = rear = -1;
+        max = capacity;
+        size = 0;
+    }
+
+    public void enqueue(int data){
+        if (isFull()){
+            System.out.println("queue is full");
+            return;
+        }
+        if(front == -1) front++;
+        rear = (rear+1) % max;
+        arr[rear] = data;
+        size++;
+    }
+    public boolean isFull(){
+        return size == max;
+    }
+
+    public int dequeue(){
+        if(isEmpty()){
+            System.out.println("queue is empty");
+            return -1;
+        }
+        int x = arr[front];
+        if(front == rear)
+            front = rear = -1;
+        front = (front+1) % max;
+        size--;
+        return x;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void display(){
+        for(int i = front; i<= rear;  i++){
+            System.out.print(i+" ");
+        }
+    }
+
+    public static void main(String args[]){
+        CircularQueue cl = new CircularQueue(5);
+        cl.enqueue(1);
+        cl.enqueue(2);
+        cl.enqueue(3);
+        cl.enqueue(4);
+        cl.display();
+
+    }
+
+}
